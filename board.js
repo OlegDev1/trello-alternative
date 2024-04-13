a581146ed70fd01cc4b4a5acccf14e1a47062bdf
Fykapu <jsjwhshjsig@gmail.com>
2024-04-13 11:12:14 +0200

    Delete cards functionality, data is now stored in array

--- board.js
+++ board.js
@@ -1,30 +1,30 @@
 //Board clicks handler
 function boardClickHandler(event) {
     if (event.target.closest(".list__addItem")) return addNewItemTextarea(event);
-    if (event.target.className == 'list-addList') {
-        document.querySelector(".list-add").insertAdjacentHTML("beforebegin", listItem);
-    }
+    if (event.target.className == "list-addList") return addNewList(event);
 }
 document.querySelector(".board").addEventListener("click", boardClickHandler);
 
 // Add a list
-
-let listItem = `<li class="list-wrapper">
-<div class="list">
-    <header class="list__header">
-        <h2 class="list__name">List name</h2>
-        <button class="list__settings">
-            <img class="icon" src="imgs/list-settings-dots.png">
+function addNewList(event) {
+    let listItem = `<li class="list-wrapper">
+    <div class="list">
+        <header class="list__header">
+            <h2 class="list__name">List name</h2>
+            <button class="list__settings">
+                <img class="icon" src="imgs/list-settings-dots.png">
+            </button>
+        </header>
+        <ol class="list__items"></ol>
+        <button class="list__addItem">
+            <img class="icon" src="imgs/list-item-add-plus.png">
+            Add a card
         </button>
-    </header>
-    <ol class="list__items"></ol>
-    <button class="list__addItem">
-        <img class="icon" src="imgs/list-item-add-plus.png">
-        Add a card
-    </button>
-</div>
-</li>`;
-
+    </div>
+    </li>`;
+    document.querySelector(".list-add").insertAdjacentHTML("beforebegin", listItem);
+    lists.push(new List());
+}
 
 // Add a card to the list
 function addNewItemTextarea(event) {
@@ -37,12 +37,12 @@ function addNewItemTextarea(event) {
     <button class="list_addItem-cancel"><img class="icon" src="imgs/addItem-cancel-cross.png"></button></div>`;
 
     list.querySelector(".list__item-addText").addEventListener("blur", (event) => {
-        if (event.relatedTarget == document.querySelector(".list__addItem-confirm")) return addNewItem(event, true);
-        return addNewItem(event, false);
+        if (event.relatedTarget == document.querySelector(".list__addItem-confirm")) return addNewItem(list, true);
+        return addNewItem(list, false);
     });
 }
 
-function addNewItem(event, confirm) {
+function addNewItem(list, confirm) {
     let textarea = document.querySelector(".list__item-addText");
     if (confirm) {
         textarea.closest(".list__item").innerHTML = `<span class="list__item-content">
@@ -51,6 +51,9 @@ function addNewItem(event, confirm) {
         <button class="item__content-edit">
             <img class="icon" src="imgs/list-item-edit-pencil.png">
         </button>`;
+        let currentList = list.closest(".list-wrapper");
+        let curerntListIndex = Array.from(currentList.parentNode.children).indexOf(currentList);
+        lists[curerntListIndex].addItem(textarea.value)
     } else {
         textarea.closest(".list__item").remove();
     }
@@ -58,5 +61,56 @@ function addNewItem(event, confirm) {
     document.querySelector(".list__addItem__buttons").outerHTML = `  <button class="list__addItem">
     <img class="icon" src="imgs/list-item-add-plus.png">
     Add a card
-</button>`;
+    </button>`;
 }
+
+//list class
+let lists = [];
+class List {
+    constructor() {
+        this.items = [];
+    }
+    addItem(item) {
+        this.items.push(item);
+    }
+    removeItem(index) {
+        this.items.splice(index, 1);
+    }
+}
+
+// Delete a card from the list
+function contextMenuHandler(event) {
+    let listItem = event.target.closest('.list__item');
+    if (!listItem) return;
+    let listItemIndex = Array.from(listItem.parentNode.children).indexOf(listItem);
+    let listIndex = Array.from(event.target.closest('.list-wrapper').parentNode.children).indexOf(event.target.closest('.list-wrapper'));
+
+    event.preventDefault()
+
+    let div = document.createElement('div');
+    div.className = 'contextMenuBackground';
+    document.body.append(div);
+    listItem.classList.add('focused')
+
+    //on click on the background
+    div.addEventListener('click', function() {
+        this.remove();
+        listItem.classList.remove('focused');
+        document.querySelector('.list__item-contextMenu').remove()
+    });
+    div.addEventListener('contextmenu', (event) => event.preventDefault())
+
+    let contextMenu = document.createElement('div');
+    contextMenu.className = 'list__item-contextMenu';
+    contextMenu.textContent = 'Delete'
+    listItem.append(contextMenu);
+    //on click on the remove button
+    contextMenu.addEventListener('click', function(event) {
+        document.querySelector('.contextMenuBackground').remove();
+        listItem.remove();
+        lists[listIndex].removeItem(listItemIndex);
+    });
+}
+
+
+document.addEventListener('contextmenu', contextMenuHandler)
